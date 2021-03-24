6f6900f58b71f447cb4408ab3a79a1190a0a0418
t4h4 <tahayasin@outlook.com>
2021-03-24 17:03:50 +0300

    Custom Validator eklendi. ( Must fonksiyonu yardımıyla. )

--- FluentValidationApp/FluentValidators/CustomerValidator.cs
+++ FluentValidationApp/FluentValidators/CustomerValidator.cs
@@ -11,16 +11,24 @@ namespace FluentValidationApp.FluentValidators
     //AbstractValidator generic sınıf. generic olacak neyle uğraşacağını belirtmemiz gerek. 
     public class CustomerValidator : AbstractValidator <Customer>
     {
+        public string NotEmptyMessage { get; } = "{PropertyName} alani boş olamaz.";
         public CustomerValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Name alani bos olamaz.");
+            RuleFor(x => x.Name).NotEmpty().WithMessage(NotEmptyMessage);
 
-            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alani bos olamaz.")
+            RuleFor(x => x.Email).NotEmpty().WithMessage(NotEmptyMessage)
                 .EmailAddress().WithMessage("Email dogru formatta olmalidir.");
 
-            RuleFor(x => x.Age).NotEmpty().WithMessage("Age alani bos olamaz.").InclusiveBetween(18, 60)
+            RuleFor(x => x.Age).NotEmpty().WithMessage(NotEmptyMessage).InclusiveBetween(18, 60)
                 .WithMessage("Age alani 18 ile 60 arasinda olmalidir.");
 
+            //custom validator kullanmak için must metodunu kullan.
+            //custom hatalar client tarafta görülmez. mutlaka verinin server tarafa gitmesi gerekir.
+            //x girilen değeri alıyor.
+            RuleFor(x => x.Birthday).NotEmpty().WithMessage(NotEmptyMessage).Must(x =>
+            {
+                return DateTime.Now.AddYears(-18) >= x; //şu anki yıldan 18 yıl geriye git, x'ten büyük veya eşitse true yoksa false. False ise 18 yaşından küçüktür. 
+            }).WithMessage("Yaşınız 18 yaşından büyük olmalıdır");
         }
     }
 }
--- FluentValidationApp/Models/Customer.cs
+++ FluentValidationApp/Models/Customer.cs
@@ -13,5 +13,7 @@ namespace FluentValidationApp.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public int Age { get; set; }
+        public DateTime? Birthday { get; set; }
+
     }
 }
